8035111defb59e66ec892c5f0cc8289fa0fbd922
Artur <arturnerictt@gmail.com>
2023-03-28 14:44:09 -0300

    extraindo responsabilidades

--- carro.js
+++ carro.js
@@ -1,30 +1,30 @@
 
-let xCarros = [600, 600, 600]
-let yCarros = [40, 96, 150]
-let velocidadeCarros = [2, 2.5, 3.2]
+let xCarros = [600, 600, 600];
+let yCarros = [40, 96, 150];
+let velocidadeCarros = [2, 2.5, 3.2];
 
 
 
 function mostraCarro(){
   for(let i = 0; i < imagemCarros.length; i++){
-    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 30)
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 30);
   }
 }
   
 function movimentaCarro(){
-    xCarros[0] -= velocidadeCarros[0]
-    xCarros[1] -= velocidadeCarros[1]
-    xCarros[2] -= velocidadeCarros[2]
+  for(let i = 0; i<imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
 }
 
 function voltaPosicaoInicialDoCarro(){
-  if (xCarros[0] < -50) {
-    xCarros[0] = 600;
+  for(let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
   }
-  if (xCarros[1] < -50) {
-    xCarros[1] = 600;
-  }
-  if (xCarros[2] < -50) {
-    xCarros[2] = 600;
-  }
+}
+
+function passouTodaATela(xCarro) {
+  return xCarro < -50;
 }
