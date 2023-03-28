6597556595d184421a27fdc6f68827088df6a29d
Artur <arturnerictt@gmail.com>
2023-03-28 16:38:20 -0300

    adicionando sons, validacao de movimento e finalizando o jogo

--- atriz.js
+++ atriz.js
@@ -1,7 +1,8 @@
 //ator
 let yAtriz = 350;
-let xAtriz = 100;
+let xAtriz = 85;
 let colisao = false
+let meusPontos = 0
 
 function mostraAtor(){
     image(imagemDoAtor, xAtriz, yAtriz, 45, 45);
@@ -12,20 +13,49 @@ function mostraAtor(){
       yAtriz -= 3
     }
     if (keyIsDown(DOWN_ARROW)){
-      yAtriz += 3
+      if (podeSeMover()){
+        yAtriz += 3
+      }
     }
   }
 
   function verificaColisao(){
     // collideRectRect(x, y, width, height, x2, y2, width2, height2 )
     for (let i = 0; i < imagemCarros.length; i++){
-      colisao = collideRectRect(xAtriz, yAtriz, 45, 45, xCarros[i], yCarros[i], 50, 30)
+      colisao = collideRectRect(xAtriz, yAtriz, 35, 35, xCarros[i], yCarros[i], 50, 30)
       if (colisao){
-        colidiu()
+        voltaParaPosicalInicial();
+        somDaColisao.play();
+        if (pontosMaiorQueZero()){
+          meusPontos -= 1;
+        }
       }
     }
   }
 
-  function colidiu(){
-    yAtriz = 350
+  function voltaParaPosicalInicial(){
+    yAtriz = 350;
+  }
+
+  function incluiPontos(){
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255,240,60));
+    text(meusPontos, width / 5, 27);
+  }
+
+  function marcaPonto(){
+    if (yAtriz < 12) {
+      meusPontos += 1;
+      voltaParaPosicalInicial();
+      somDoPonto.play();
+    }
+  }
+
+  function pontosMaiorQueZero() {
+    return meusPontos > 0;
+  }
+
+  function podeSeMover(){
+    return yAtriz < 350;
   }
